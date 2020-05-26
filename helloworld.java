6643db5749784c32e413338ac115db3edde9e79e
xiaoyang0521 <752347212@qq.com>
2020-05-26 20:59:46 +0800

    hello world2

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("4");
+        System.out.println("Hello World");
+        System.out.println("5");
+        System.out.println("Hello World");
+
+    }
+}
